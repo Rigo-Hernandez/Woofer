e6e00698ee1a27ec1ca97dc2b72a37a91b63cdcd
Rigo-Hernandez <hernandez_rigoberto@outlook.com>
2019-03-22 15:25:26 -0500

    route redirection fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ class App extends Component {
   render() {
     return (
       <Provider store ={store}>
-      
       <div className="App">
         <Navbar />
         <header className="App-header">
